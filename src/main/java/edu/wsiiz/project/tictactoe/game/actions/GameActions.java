fad0e3b0928e9f12e2993ded68d02cec2a2611dd
Dominika Matyka <dominika.mmatyka@gmail.com>
2022-12-21 21:07:03 +0100

    add menu and board

---
+++ src/main/java/edu/wsiiz/project/tictactoe/game/actions/GameActions.java
@@ -0,0 +1,12 @@
+package edu.wsiiz.project.tictactoe.game.actions;
+
+import edu.wsiiz.project.tictactoe.util.Level;
+import edu.wsiiz.project.tictactoe.util.Sign;
+
+public class GameActions {
+    public static void play(Level level, Sign chooseSign) {
+    }
+
+    public static void endGame() {
+    }
+}
---
+++ src/main/java/edu/wsiiz/project/tictactoe/util/Constants.java
@@ -0,0 +1,9 @@
+package edu.wsiiz.project.tictactoe.util;
+
+import java.util.List;
+
+public class Constants {
+    public static final List<String> LEVEL_OPTIONS = List.of("1", "2", "3");
+    public static final List<String> SIGN_OPTIONS = List.of("X", "O", "x", "o");
+    public static final List<String> ACTION_OPTIONS = List.of("1", "2");
+}
