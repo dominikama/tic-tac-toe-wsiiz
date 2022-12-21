e4ac021bc261eadb9b60d312364c9dcd01315ec8
Dominika Matyka <dominika.mmatyka@gmail.com>
2022-12-21 14:55:34 +0100

    init spring

---
+++ src/test/java/edu/wsiiz/project/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wsiiz.project.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
