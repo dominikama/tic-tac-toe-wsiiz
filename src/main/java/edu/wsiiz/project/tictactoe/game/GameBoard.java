097e45ca2eab87741462a8564c90be3e444630fa
Dominika Matyka <dominika.mmatyka@gmail.com>
2022-12-21 15:22:36 +0100

    make skeleton

---
+++ src/main/java/edu/wsiiz/project/tictactoe/game/GameBoard.java
@@ -0,0 +1,51 @@
+package edu.wsiiz.project.tictactoe.game;
+
+import edu.wsiiz.project.tictactoe.util.Sign;
+import edu.wsiiz.project.tictactoe.util.SignOperator;
+
+public class GameBoard {
+    private static final int BOARD_SIZE = 3;
+    private char[][] board;
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public GameBoard() {
+        createEmptyBoard();
+    }
+
+    public void displayBoard() {
+        System.out.println("---------");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
+    public void modifyElement(int row, int column, char value) {
+        board[row][column] = value;
+        System.out.println("Making move " + (row + 1) + ", " + (column + 1));
+    }
+
+    public boolean isEmpty(int row, int column) {
+        return SignOperator.getIntSign(Sign.SPACE) == board[row][column];
+    }
+
+    private void createEmptyBoard() {
+        board = new char[BOARD_SIZE][BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                board[i][j] = SignOperator.getCharSign(Sign.SPACE); // filling array with spaces.
+            }
+        }
+    }
+}
